e9a01fbb7f413137ad9eae4317acfa78ab49a68b
Liam Taylor <liam.taylor888@googlemail.com>
2018-01-09 20:20:14 +0000

    Set up webpack

---
+++ app/frontend/javascripts/entry.js
@@ -0,0 +1,13 @@
+var _ = require('lodash');
+_.times(11, function(i) {
+  console.log(i);
+});
+console.log("hawwooo")
+
+const muthafucka = () => {
+  let ham = 4
+  ham = ham + 6
+  return ham
+}
+
+console.log(muthafucka())
